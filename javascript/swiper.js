c9c8670c71633906710a1a00d26be6a3164c848f
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2022-11-24 18:09:42 +0200

    Added skills section | Added swiper

---
+++ javascript/swiper.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    spaceBetween: 60,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+      el: '.skill__boxes__pagination',
+      clickable: true,
+    },
+    on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 1023 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
