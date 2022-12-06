aa474d7ba67b2dd7e550366561b06e2acee3670d
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2022-12-06 02:05:06 +0200

    Simplified portfolio filters

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,42 +1,23 @@
 function filterSelection(section) {
-  let element = document.getElementsByClassName("portfolio__box");
-  if (section == "all") {
-    section = "";
-  }
-  for (let i = 0; i < element.length; i++) {
-    RemoveClass(element[i], "portfolio__show");
-    if (element[i].className.indexOf(section) > -1)
-      AddClass(element[i], "portfolio__show");
-  }
-}
+  const portfolioElements = document.getElementsByClassName("portfolio__box");
 
-function AddClass(element, name) {
-  let arr1 = element.className.split(" ");
-  let arr2 = name.split(" ");
-  for (let i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+  for (let i = 0; i < portfolioElements.length; i++) {
+    const currentElement = portfolioElements[i];
+
+    if (currentElement.classList.contains(section) || section === "") {
+      currentElement.classList.add("portfolio__show");
+    } else {
+      currentElement.classList.remove("portfolio__show");
     }
   }
 }
 
-function RemoveClass(element, name) {
-  let arr1 = element.className.split(" ");
-  let arr2 = name.split(" ");
-  for (let i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-let btnContainer = document.getElementsByClassName("portfolio__btns");
-let btns = btnContainer[0].getElementsByClassName("portfolio__btn");
+const btnContainer = document.getElementsByClassName("portfolio__btns");
+const btns = btnContainer[0].getElementsByClassName("portfolio__btn");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("btn__active");
-    current[0].className = current[0].className.replace(" btn__active", "");
-    this.className += " btn__active";
+    let activeButton = document.getElementsByClassName("btn__active");
+    activeButton[0].classList.remove("btn__active");
+    this.classList.add("btn__active");
   });
 }
